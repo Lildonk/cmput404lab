f6c644c958715162785f7b8c5d70c64f84fe3b83
zlv <zlv@ualberta.ca>
2022-01-14 18:04:12 -0500

    3

--- requestsVersion.py
+++ requestsVersion.py
@@ -3,5 +3,5 @@ import requests as r
 print(r.__version__)
 res = r.get('http://www.google.com/')
 print(res.text)
-res2 = r.get('http://github.com/Lildonk/cmput404lab.git')
+res2 = r.get('http://raw.githubusercontent.com/Lildonk/cmput404lab/master/requestsVersion.py')
 print(res2.text)
